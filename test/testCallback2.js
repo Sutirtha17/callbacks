0b84d07b85be2fd621fb8813f8fd8d3351755639
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-09-14 16:44:20 +0530

    changed json file name from list to lists

--- test/testCallback2.js
+++ test/testCallback2.js
@@ -1,7 +1,7 @@
-const list = require('../lists.json');
+const lists = require('../lists.json');
 const listOfBoardInformation = require('../callback2.js');
 
-listOfBoardInformation('mcu453ed', list, (error, listData) => {
+listOfBoardInformation('mcu453e', lists, (error, listData) => {
   if (listData) console.log(listData);
   else console.log(error);
 });
