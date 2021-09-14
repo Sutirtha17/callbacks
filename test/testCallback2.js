3cf45611b22faf4ab4b108d0a1b4e20d7aea285f
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-09-14 17:15:23 +0530

    changed the input ID on lisOfBoardInformation function

--- test/testCallback2.js
+++ test/testCallback2.js
@@ -1,7 +1,7 @@
 const lists = require('../lists.json');
 const listOfBoardInformation = require('../callback2.js');
 
-listOfBoardInformation('mcu453e', lists, (error, listData) => {
+listOfBoardInformation('mcu453ed', lists, (error, listData) => {
   if (listData) console.log(listData);
   else console.log(error);
 });
