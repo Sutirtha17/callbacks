e3fcd2a8f324a8c116ae8d778b5caf31352650a5
Sutirtha Dey <sutirtha.dey.17@mountblue.tech>
2021-09-14 16:38:07 +0530

    added error handling statements

--- callback1.js
+++ callback1.js
@@ -10,15 +10,17 @@
 /* 
 	Problem 1: Write a function that will return a particular board's information based on the boardID that is passed from the given list of boards in boards.json and then pass control back to the code that called it by using a callback function.
 */
+
 const getInformationOfBoards = (boardId, boards, cb) => {
   setTimeout(() => {
-    let boardData;
-    try {
-      boardData = boards.filter((board) => board.id == boardId);
-    } catch {
-      cb(new Error('Invalid json format, please give correct dataset'));
+    let boardData = boards.filter((board) => board.id == boardId);
+
+    if (boardData) {
+      cb(null, boardData);
+    } else {
+      let error = new Error('Invalid ID or Json file!');
+      cb(error);
     }
-    cb(null, boardData);
   }, 2000);
 };
 
